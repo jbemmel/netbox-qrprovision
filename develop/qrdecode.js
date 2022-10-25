148dc4dae6073831cdfa4a8a2a59f31afca0f8cc
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-25 15:14:50 -0500

    Find start/end of record

--- develop/qrdecode.js
+++ develop/qrdecode.js
@@ -3,6 +3,11 @@
 const QR = '[)>\x1E06\x1D1P3HE12345AARA01\x1DSNK214901234\x1D18VLENOK\x1DD211207\x1D1VNKS\x1D11PABCDEFGHIJ\x1D4LMX\x1D2P01\x1E\x04'
 
 const GS = String.fromCharCode(29)
+const RS = String.fromCharCode(30)
+
+const start = QR.indexOf(RS)
+const end = QR.indexOf(RS,start+1)
+console.log( `Start: ${start} End: ${end}` )
 
 const DECODE = {
   "1P" : "Part number",
@@ -15,7 +20,7 @@ const DECODE = {
   "4L" : "Country of origin"
 }
 
-for (value of QR.split(GS)) {
+for (value of QR.substr(start,end).split(GS)) {
   console.log(`Field: ${value}`)
   for (const p in DECODE) {
     if (value.startsWith(p)) {
